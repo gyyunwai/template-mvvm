ba46e16605db4ba4fe221c23f9ae29c2d2f5a099
GitHub Action <action@github.com>
2021-07-11 13:42:07 +0000

    Template cleanup

--- src/main/kotlin/com/github/gyyunwai/templatemvvm/services/MyProjectService.kt
+++ src/main/kotlin/com/github/gyyunwai/templatemvvm/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.gyyunwai.templatemvvm.services
 
+import com.github.gyyunwai.templatemvvm.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
